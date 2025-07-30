ffef2e172cc521dcd5ce23a004ed9f62c5b01c56
Arianna Method <theariannamethod@gmail.com>
2025-07-30 05:17:19 +0300

    Delete Evaluation/MBPP directory

--- Evaluation/MBPP/eval.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-MODEL_NAME_OR_PATH="PATH_TO_LOCAL_MODEL"
-DATASET_ROOT="data/"
-LANGUAGE="python"
-CUDA_VISIBLE_DEVICES=1,2,3 python -m accelerate.commands.launch --config_file test_config.yaml eval_pal.py --logdir ${MODEL_NAME_OR_PATH} --language ${LANGUAGE} --dataroot ${DATASET_ROOT}
